98a1bc04115eac8b2ea83b7d4185451a070f7374
Alex Hughes <ahughesalex@gmail.com>
2020-12-30 01:20:24 -0800

    Add support for a Null Operator

--- path_template/builtin/operators/__init__.py
+++ path_template/builtin/operators/__init__.py
@@ -1,8 +1,10 @@
 from .lower import LowerOperator
 from .upper import UpperOperator
 from .substr import SubStringOperator
+from .null import NullOperator
 ALL_OPERATORS = [
     LowerOperator,
     UpperOperator,
     SubStringOperator,
+    NullOperator,
 ]
---
+++ path_template/builtin/operators/null.py
@@ -0,0 +1,11 @@
+from path_template.operator import Operator
+
+
+class NullOperator(Operator):
+    name = "NULL"
+
+    def is_invalid(self, token_value):
+        return None
+
+    def execute(self, input_data):
+        return input_data
