2fa7c931965b7a5267cd2d01c66dcb3e0c69dea9
Alex Hughes <ahughesalex@gmail.com>
2020-12-29 23:36:59 -0800

    Rename `construct_template` to `resolve_template`
    We also finish off the `parse` method to return the resolved data from the template_str.

--- path_template/parser.py
+++ path_template/parser.py
@@ -15,11 +15,14 @@ class PathTemplateParser(object):
 
     def parse(self, template, data):
         if not isinstance(template, Template):
-            template = self.construct_template(template)
+            template = self.validate_template(template)
 
-    def construct_template(self, template_str):
+        return template.resolve(data)
+
+    def validate_template(self, template_str):
         template = Template.from_template_str(
             template_str=template_str,
             tmanager=self._token_manager,
             omanager=self._operator_manager
         )
+        return template
