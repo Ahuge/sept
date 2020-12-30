d8ab085c6a5d63494cbdf14a133a58dc55baa39c
Alex Hughes <ahughesalex@gmail.com>
2020-12-29 23:24:25 -0800

    Implement a specific pyparsing PEG parser
    It will build us a `Node` object and construct a deep tree object for the resulting Node objects

---
+++ path_template/template_tokenizer.py
@@ -0,0 +1,92 @@
+import pyparsing
+
+token_group_start = pyparsing.Literal("{{")
+token_group_end = pyparsing.Literal("}}")
+operator_name = pyparsing.Word(pyparsing.alphanums)("Operator")
+operator_args = pyparsing.delimitedList(
+    pyparsing.OneOrMore(
+        pyparsing.Word(pyparsing.printables, excludeChars="[],")
+    )
+)("Args")
+
+operator_arg_group = (
+    pyparsing.Literal("[") +
+    operator_args +
+    pyparsing.Literal("]")
+)
+
+operator = (
+    operator_name +
+    pyparsing.Optional(operator_arg_group,) +
+    pyparsing.Literal(":")
+)
+token_name = pyparsing.Word(pyparsing.alphanums, excludeChars="{}[]:")("Token")
+
+
+class Node(object):
+    def __init__(self, Operator, Args, Token, length, tok_start, tok_str, child=()):
+        super(Node, self).__init__()
+        self.Operator = Operator
+        self.Args = Args
+        self.Token = Token
+        self.child = child
+        self.length = length
+        self.start = int(tok_start)
+        self.end = int(tok_start + self.length)
+        self.original_str = tok_str
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        msg = "<Node "
+        if self.Operator:
+            msg += "Operator:{} ".format(self.Operator)
+        if self.Args:
+            msg += "Args:{} ".format(self.Args)
+        if self.Token:
+            msg += "Token:{}".format(self.Token)
+        elif self.child:
+            msg += "Child:{}".format(self.child)
+        msg += ">"
+        return msg
+
+
+def parseAction(string, location, tokens):
+    operator = tokens.Operator
+    operator_args = tokens.Args
+    token = tokens.Token or tokens[-2]
+    length = sum(map(len, tokens))
+    end = location + length
+    if isinstance(token, Node):
+        new_node = Node(
+            child=token,
+            Operator=operator,
+            Args=operator_args,
+            Token=None,
+            length=length,
+            tok_start=location,
+            tok_str=string[location:end]
+        )
+    else:
+        new_node = Node(
+            child=None,
+            Operator=operator,
+            Args=operator_args,
+            Token=token,
+            length=length,
+            tok_start=location,
+            tok_str=string[location:end]
+        )
+    return new_node
+
+
+Tokenizer = pyparsing.Forward().setResultsName("recursive_expr")
+
+Tokenizer << (
+    token_group_start +
+    pyparsing.Optional(operator, default=None) +
+    (Tokenizer | token_name) +
+    token_group_end
+)
+Tokenizer.setParseAction(parseAction)
