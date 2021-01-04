7667d099d2d855a3e4625625b8943f278a7a18c9
Alex Hughes <ahughesalex@gmail.com>
2021-01-04 09:12:12 -0800

    Implement the `replace` operator
    This Operator replaces `srcChars` with `dstChars`. It supports `\s` as a space token.

--- sept/builtin/operators/__init__.py
+++ sept/builtin/operators/__init__.py
@@ -1,10 +1,12 @@
 from .lower import LowerOperator
 from .upper import UpperOperator
 from .substr import SubStringOperator
+from .replace import ReplaceOperator
 from .null import NullOperator
 ALL_OPERATORS = [
     LowerOperator,
     UpperOperator,
     SubStringOperator,
     NullOperator,
+    ReplaceOperator,
 ]
