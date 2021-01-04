36270bc83ef8c5d0a9e9d0c370b1f6f1425d747a
Black Code Formatter <black_code_formatter@gmail.com>
2021-01-04 19:12:24 +0000

    Black Automatically Formatted Code

--- sept/__init__.py
+++ sept/__init__.py
@@ -3,5 +3,6 @@ from .operator import Operator
 from .parser import PathTemplateParser
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
