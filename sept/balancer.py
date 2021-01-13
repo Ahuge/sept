03703994be705004c11f6a881d6cb0f2048d0a41
Alex Hughes <ahughesalex@gmail.com>
2021-01-13 14:58:37 -0800

    Update our BalancingParenthesisError accuracy
    The two BalancingParnthesisError classes have had some update to attempt to find the most relevant single open/close token for them.
    This may cause issues in nested Token Expressions because it will pick the closes.
    TODO: Remove matched ones from the list maybe?

--- sept/balancer.py
+++ sept/balancer.py
@@ -15,12 +15,19 @@ class ParenthesisBalancer(object):
         self._root_locations = []
         self._currently_open_locations = []
         self._recently_closed_locations = []
+        self._single_opens = []
+        self._single_closes = []
         self._errors = []
 
     def execute(self):
         # msg = " {} ".format(msg)
         for index, curr_char in enumerate(self.template_str):
             prev_char, next_char = self._get_chars(curr_index=index)
+
+            if curr_char == START_TOK:
+                self._single_opens.append(index)
+            elif curr_char == CLOSE_TOK:
+                self._single_closes.append(index)
 
             if self._is_start_tok(curr_char, next_char):
                 # Might have a tok_start
@@ -42,10 +49,15 @@ class ParenthesisBalancer(object):
                     self.close_group(curr_index=index)
         if self._currently_open_locations:
             for open_location in self._currently_open_locations:
+                next_close = open_location
+                for close_index in reversed(self._single_closes):
+                    if close_index < open_location:
+                        break
+                    next_close = close_index
                 self._errors.append(
                     ClosingBalancingParenthesisError(
                         start_location=open_location,
-                        end_location=len(self.template_str) - 1,
+                        end_location=next_close,
                         missing_token=CLOSE_TOK + CLOSE_TOK,
                         substr=self.template_str[open_location:],
                     )
@@ -81,12 +93,19 @@ class ParenthesisBalancer(object):
             if self._recently_closed_locations:
                 last_closed = self._recently_closed_locations[-1]
 
+            last_open = last_closed + 2
+            for open_index in reversed(self._single_opens):
+                if open_index < last_closed:
+                    break
+                last_open = open_index
+
+            end_index = curr_index + 1
             self._errors.append(
                 OpeningBalancingParenthesisError(
-                    start_location=last_closed + 1,
-                    end_location=curr_index,
+                    start_location=last_open,
+                    end_location=end_index,
                     missing_token=START_TOK + START_TOK,
-                    substr=self.template_str,
+                    substr=self.template_str[last_open:end_index + 1],
                 )
             )
             return
